778eb71fcc91b9de214356ca0dd36547033ac166
Jonny Svensson <swe_jonny@hotmail.com>
2019-09-20 00:43:27 +0200

    Added scroll down on mouseclick

--- webview.js
+++ webview.js
@@ -3,7 +3,7 @@ module.exports = Franz => {
   getMessages = () => {
     const newsDOM = document.querySelectorAll("div[title='All'] > .LeftnavListRow__count")[0].innerHTML;
     let counter = parseInt(newsDOM);
-    
+
     if (newsDOM.indexOf('K') !== -1 || newsDOM.indexOf('+') !== -1) {
       counter = newsDOM.substring(0, newsDOM.indexOf('K')) + '000';
     }
@@ -13,4 +13,12 @@ module.exports = Franz => {
 
   Franz.loop(getMessages);
 
+  //Scrolls down a page when clicking on the mouse navigate-forward button
+  document.onmousedown = function (evt) {
+    //Button 4 seems to be forwards
+    if (evt.button == 4) {
+      //Franz dosnt seem to support scrollByPage so makeing a workaround
+      window.scrollBy(0, window.innerHeight - 100);
+    }
+  };
 }
